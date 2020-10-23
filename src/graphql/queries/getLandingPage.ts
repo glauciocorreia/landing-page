9c513c3d122e9d7e98768c061eddff1f7516d896
glauciocorreia <glauciocorreiaa@gmail.com>
2020-10-22 22:24:58 -0300

    Tornando o SectionAgenda dinâmico

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,79 +1,87 @@
 const GET_LANDING_PAGE = /* GraphQL*/ `
-  fragment logo on LandingPage {
-    logo {
+fragment logo on LandingPage {
+  logo {
+    alternativeText
+    url
+  }
+}
+
+fragment header on LandingPage {
+  header {
+    title
+    description
+    button {
+      label
+      url
+    }
+    image {
       alternativeText
       url
     }
   }
+}
 
-  fragment header on LandingPage {
-    header {
+fragment sectionAboutProject on LandingPage {
+  sectionAboutProject {
+    title
+    description
+    image {
+      alternativeText
+      url
+    }
+  }
+}
+
+fragment sectionTech on LandingPage {
+  sectionTech {
+    title
+    techIcons {
       title
+      icon {
+        url
+      }
+    }
+  }
+}
+
+fragment sectionConcepts on LandingPage {
+  sectionConcepts {
+    title
+    concepts {
+      title
+    }
+  }
+}
+
+fragment sectionModules on LandingPage {
+  sectionModules {
+    title
+    modules {
+      title
+      subtitle
       description
-      button {
-        label
-        url
-      }
-      image {
-        alternativeText
-        url
-      }
     }
   }
+}
 
-  fragment sectionAboutProject on LandingPage {
-    sectionAboutProject {
-      title
-      description
-      image {
-        alternativeText
-        url
-      }
-    }
+fragment sectionAgenda on LandingPage {
+  sectionAgenda {
+    title
+    description
   }
+}
 
-  fragment sectionTech on LandingPage {
-    sectionTech {
-      title
-      techIcons {
-        title
-        icon {
-          url
-        }
-      }
-    }
+query GET_LANDING_PAGE {
+  landingPage {
+    ...logo
+    ...header
+    ...sectionAboutProject
+    ...sectionTech
+    ...sectionConcepts
+    ...sectionModules
+    ...sectionAgenda
   }
-
-  fragment sectionConcepts on LandingPage {
-    sectionConcepts {
-      title
-      concepts {
-        title
-      }
-    }
-  }
-
-  fragment sectionModules on LandingPage {
-    sectionModules {
-      title
-      modules {
-        title
-        subtitle
-        description
-      }
-    }
-  }
-
-  query GET_LANDING_PAGE {
-    landingPage {
-      ...logo
-      ...header
-      ...sectionAboutProject
-      ...sectionTech
-      ...sectionConcepts
-      ...sectionModules
-    }
-  }
+}
 `
 
 export default GET_LANDING_PAGE
--- src/types/api.ts
+++ src/types/api.ts
@@ -53,6 +53,11 @@ export type SectionModulesProps = {
   }>
 }
 
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -60,4 +65,5 @@ export type LandingPageProps = {
   sectionTech: SectionTechProps
   sectionConcepts: SectionConceptsProps
   sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
 }
